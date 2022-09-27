0ab39f999cc6bfb7eb3aa62a39a18eacb798ca91
Ajayi <ajayidavies7@gmail.com>
2022-09-26 17:34:25 -0700

    nodes with one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,14 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
-
-	if (tree)
-	{
-		nodes += (tree->left || tree->right) ? 1 : 0;
-		nodes += binary_tree_nodes(tree->left);
-		nodes += binary_tree_nodes(tree->right);
-	}
-	return (nodes);
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL && tree->left != NULL)
+		return (1);
+	if (tree->right != NULL && tree->left == NULL)
+		return (1);
+	if (tree->right != NULL && tree->left != NULL)
+		return (1 + binary_tree_nodes(tree->left) +
+				binary_tree_nodes(tree->right));
+	return (0);
 }
