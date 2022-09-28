2228e1b34cd2fffd11efeee390b0f166a18d0913
Ajayi <ajayidavies7@gmail.com>
2022-09-28 01:58:55 -0700

    ancestor checker

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - check for a common ancestor between
+ * two nodes
+ * @first: first node
+ * @second: second node
+ * Return: NULL if no ancestor or pointer to the shared ancestor
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second)
+{
+	const binary_tree_t *temp;
+
+	if (first == NULL || first->parent == NULL ||
+			second == NULL || second->parent == NULL)
+		return (NULL);
+
+	while (first)
+	{
+		temp = second;
+		while (temp)
+		{
+			temp = temp->parent;
+			if (first == temp)
+				return ((binary_tree_t *)first);
+		}
+		first = first->parent;
+	}
+	return (NULL);
+}
+
