1b712d949772171119105a74fb60a2e023ea0dfd
Ajayi <ajayidavies7@gmail.com>
2022-09-28 01:03:15 -0700

    uncle defined

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,11 +10,11 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL ||
-	    node->parent == NULL ||
-	    node->parent->parent == NULL)
+
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-	if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+
+	return (node->parent->parent->right !=
+			node->parent ? node->parent->parent->right :
+			node->parent->parent->left);
 }
